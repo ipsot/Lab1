7624ded98442e7d5273694b0dfa2217f6a4dddc4
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 12:54:41 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double x, y;
+            double a, b, p;
+            Console.WriteLine("Введите шаг");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Введите помежуток");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+
+            x = a;
+            do
+            {
+                if (x < -2)
+                {
+                    y = 5 + ( Math.Log10(3 * x)) ;
+                }
+                else if (x>-2 && x<=2)
+                {
+                    y = (Math.Cos(x) - Math.Sin(x)) ;
+                }
+                else
+                {
+                    y = Math.Abs(4 - 2 * Math.Pow(x, 5)) - Math.Sqrt(2 * x);
+
+                }
+                Console.WriteLine($"x={x}; f(x)={y}");
+                x += p;
+
+            }
+            while (x <= b + p);
+
+            Console.ReadKey();
+
+        }
+    }
+}
